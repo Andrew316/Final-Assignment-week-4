06b957769ba2601607a81a257cb881675bd35133
Andrew316 <andrewstone798@gmail.com>
2020-10-29 14:51:23 -0400

    This is for run_analysis

--- codebook.md .R
+++ codebook.md .R
@@ -5,6 +5,7 @@ they were the two metadatas of the project within the UCI Har datasets.
 In the UCI Har Dataset there are two folders that contain the information of test 
 and train. 
 
+
 Part 1 is to merge the datasets of both train and test to create one dataset 
 
 
